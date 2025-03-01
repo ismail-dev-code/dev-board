1e7b6b9e5447b00ac59535d8dd7c0db5a062c747
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-01 15:05:57 +0600

    theme color added for the btn

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const currentDate = new Date();
 todayDate.innerText = currentDate.toDateString();
 
 const buttons = document.querySelectorAll(".btn-primary");
-const cardTitle = document.querySelectorAll(".card-title")
+const cardTitle = document.querySelectorAll(".card-title");
+const historyLog = document.getElementById("history");
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
     const taskAssigned = document.getElementById("task-assign");
     const taskComplete = document.getElementById("task-complete");
-    
 
     if (parseInt(taskAssigned.innerText) <= 0) {
       alert("No tasks remaining!");
@@ -36,18 +36,37 @@ for (let i = 0; i < buttons.length; i++) {
       buttons[i].classList.remove("btn-primary");
     }
 
-    const historyLog = document.getElementById("history");
     const newElement = document.createElement("div");
-    
+
     newElement.innerHTML = `
     <div class="bg-slate-100 p-3 mb-7 rounded-xl"> 
-        <p> You have Complete The Task ${cardTitle[i].innerText} at ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()} ${
+        <p> You have Complete The Task ${
+          cardTitle[i].innerText
+        } at ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()} ${
       currentDate.getHours() >= 12 ? "PM" : "AM"
     } </p>
     </div>
     `;
 
     historyLog.appendChild(newElement);
-
   });
 }
+
+document.getElementById("clear-btn").addEventListener("click", function () {
+  historyLog.innerText = "";
+});
+
+
+const bodyId = document.getElementById('body');
+
+const colors = ["green-500", "tomato-300", "black-50", "yellow-500"];
+document.getElementById("theme").addEventListener("click", function () {
+    const randomIndex = Math.round(Math.random() *4);
+  
+    
+    bodyId.classList.remove(...colors.map(color => `bg-${color}`));
+  
+   
+    bodyId.classList.add(`bg-${colors[randomIndex]}`);
+  }
+);
